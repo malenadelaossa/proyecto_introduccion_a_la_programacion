4681eca04622c447ea2e66c6126d6d02d07950a7
Malena de la Ossa <malenadelaossa@hotmail.com>
2020-02-20 00:50:18 -0600

    utils y game html

--- hangman/utils/__init__.py
+++ hangman/utils/__init__.py
@@ -8,14 +8,19 @@ def game_lose(tries):
     return tries== constants.MAX_TRIES
 
 
-def find_indexes(input, word):
+def find_indexes_in_string(input, word):
     indexes = []
     word_length= len(word)
     if(letter == input):
         for index, letter in range(word_length):
             if word [index] == letter:
                 indexes.append(index)
-    return indexes
+                return indexes
+
+def update_letter (letters, find_indexes):
+    for index in find_indexes:
+        letters [index] = index
+        return letters
 
 def game_won(letters):
     return "_" not in letters
